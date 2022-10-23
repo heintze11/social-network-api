4e66f5129a945ac6fc3f46a5350ac7cfc85925ff
Levi H <levi.heintzelman@gmail.com>
2022-10-23 10:24:14 -0600

    Add more files, fill out server.js and connection.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require model
-const { Item } = require('./models');
+const routes = require('./routes');
+
+// const cwd = process.cwd();
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes);
 
 
 db.once('open', () => {
@@ -16,3 +18,8 @@ db.once('open', () => {
       console.log(`API server running on port ${PORT}!`);
     });
   });
+
+  // Create models for data structure
+  // create routes to retrieve data
+  // connect everything
+  
