4072ecfcebf4e5b77e6d5aeabf7d8d79cc201522
Levi H <levi.heintzelman@gmail.com>
2022-10-27 15:38:58 -0600

    Finished and tested all User Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
   getUsers(req, res) {
@@ -22,4 +22,32 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+
+  // Delete user based on ID - Also delete associated Thoughts
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((user) =>
+    !user
+      ? res.status(404).json({ message: 'No user with that ID' })
+      : Thought.deleteMany({ _id: { $in: user.thoughts } })
+  )
+  .then(() => res.json({ message: 'User and Thoughts deleted!' }))
+  .catch((err) => res.status(500).json(err));
+  },
+
+  // Update user
+  updateUser(req, res){
+    User.findOneAndUpdate(
+      {_id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+    .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
 };
--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,23 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+// create reaction schema - has to be first since thoughtSchema references it
+const reactionSchema = new Schema(
+    {
+        reactionText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+    }
+);
 
 // schema to create though model
 const thoughtSchema = new Schema(
@@ -18,32 +37,21 @@ const thoughtSchema = new Schema(
         },
         // add reaction schema
         reactions: [reactionSchema]
+    },
+    {
+        // Add virtuals to thought model
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
     }
 );
 
-// create reaction schema
-const reactionSchema = new Schema(
-    {
-        reactionText: {
-            type: String,
-            required: true,
-            maxLength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        userName: {
-            type: String,
-            required: true,
-        },
-    }
-);
+
 
 thoughtSchema.virtual('reactionCount').get(function(){
     return this.reactions.length;
 })
 
 const Thought = model('Thought', thoughtSchema);
-
 module.exports = Thought;
